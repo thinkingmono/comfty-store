fa9bbcf28e341640050ccab8313883d05cc1562e
Alejandro Garcia <thinkingmono92@gmail.com>
2023-10-22 18:23:14 -0500

    My approach | Pages components creation, export from index.js and import to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { About, Cart, Checkout, Error, HomeLayout, Landing, Login, Orders, Products, Register, SingleProduct } from "./pages/index"
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
   )
 }
 
