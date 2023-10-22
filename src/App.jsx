d234feb3b069eb004672b1bf753b2203f4b2bfc6
Alejandro Garcia <thinkingmono92@gmail.com>
2023-10-22 18:49:09 -0500

    My approach | Adding React Router configuration to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,40 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom" /*React Router Imports*/
 import { About, Cart, Checkout, Error, HomeLayout, Landing, Login, Orders, Products, Register, SingleProduct } from "./pages/index"
+
+/*Router and routes creation*/
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/checkout',
+        element: <Checkout />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
+      {
+        path: '/products',
+        element: <Products />,
+      },
+    ]
+  }
+])
 
 function App() {
   return (
@@ -6,6 +42,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <RouterProvider router={router} /> {/*Pass in router configuration to Router Provider */}
     </>
   )
 }
