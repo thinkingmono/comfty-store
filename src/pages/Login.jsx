a8c467bb5120ec78883235053750858f614739e3
Alejandro Garcia <thinkingmono92@gmail.com>
2023-10-22 22:21:24 -0500

    Login.jsx | Delete brackets from props pass in FormInput as they are text

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,10 +6,10 @@ const Login = () => {
     <section className="h-screen grid place-items-center">
       <Form method="POST" className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
         <h4 className="text-center text-3xl font-bold">Login</h4>
-        <FormInput label={'Email'} name={'identifier'} type={'email'} defaultValue={'test@test.com'} />
-        <FormInput label={'Password'} name={'password'} type={'password'} defaultValue={'secret'} />
+        <FormInput label='Email' name='identifier' type='email' defaultValue='test@test.com' />
+        <FormInput label='Password' name='password' type='password' defaultValue='secret' />
         <div className="mt-4">
-          <SubmitBtn text={'Login'} />
+          <SubmitBtn text='Login' />
         </div>
         <button type="button" className="btn btn-secondary btn-block">Guest User</button>
         <p className="text-center">
