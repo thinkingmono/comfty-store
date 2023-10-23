a539d4cc96a00036842ecaa06a1a53f0e1032177
Alejandro Garcia <thinkingmono92@gmail.com>
2023-10-22 22:11:19 -0500

    Login Form Structure | FormInput.jsx component creation using daisyui btn template and paramters from props. index.js | Export FormInput and SubmitBtn components. SubmitBtn.jsx component creationusing useNavigation hook to identify submit btn state and conditional render logic based on submitting. Login.jsx | Adding Form & Link from react router and FormInput & SubmitBtn components imports. Create Form structure using tailwind classes.

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,13 @@
+
+const FormInput = ({ label, name, type, defaultValue }) => {
+    return (
+        <div className="form-control">
+            <label className="label">
+                <span className="label-text">{label}</span>
+            </label>
+            <input type={type} name={name} className="input input-bordered" defaultValue={defaultValue}/>
+        </div>
+    )
+}
+
+export default FormInput
