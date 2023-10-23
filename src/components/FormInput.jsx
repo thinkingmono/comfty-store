8467b3abaa6b71064d5031e21997cdba8b892098
Alejandro Garcia <thinkingmono92@gmail.com>
2023-10-22 22:54:20 -0500

    FormInput.jsx | Add capitalize class to label's input. Register.jsx | Register form creation using Form & Link from react-router and FormInput & SubmitBtn custom components. Add form register structure using tailwind/daisyui template and classes

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -3,7 +3,7 @@ const FormInput = ({ label, name, type, defaultValue }) => {
     return (
         <div className="form-control">
             <label className="label">
-                <span className="label-text">{label}</span>
+                <span className="label-text capitalize">{label}</span>
             </label>
             <input type={type} name={name} className="input input-bordered" defaultValue={defaultValue}/>
         </div>
