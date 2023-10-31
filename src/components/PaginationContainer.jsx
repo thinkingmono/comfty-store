2922284cc4e50be8b8a6f876b318eb5be62bcde4
Alejandro Garcia <thinkingmono92@gmail.com>
2023-10-31 10:21:32 -0500

    PaginationContainer.jsx | useLoaderData to capture meta data from products page loader. Destructure page and pageCount from meta.pagination data. useLocation to extract search and path from the current url. Create a navigate variable to store useNavigate function that will allow to navigate to selected page in pagination buttons. Create a pages array from quantity of pages used for the products query, it is reflected on pagination buttons at the bottom of the page. handlePageChange function to capture current search params from the url, set new search param page to pageNumber pass in to the function and navigate to current pathname from location with current search params and new page number. Enable pagintation component using tailwind/daisyui and adding handlePageChange logic for every button depends on the case.

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,7 +1,50 @@
+import { useEffect } from "react";
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom"
 
 const PaginationContainer = () => {
+  const { meta } = useLoaderData();
+  // console.log(meta);
+  const { page, pageCount } = meta.pagination;
+  const { search, pathname } = useLocation();
+  // console.log(location);
+  const navigate = useNavigate();
+  // console.log(navigate);
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    const pageIndex = index + 1;
+    return pageIndex
+  })
+  // console.log(pages);
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    // console.log(searchParams);
+    searchParams.set('page', pageNumber);
+    // console.log(searchParams);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  }
+
+  if (pageCount < 2) return null
+
   return (
-    <div>PaginationContainer</div>
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button type="button" className="btn btn-xs sm:btn-md join-item" onClick={() => {
+          let prevPage = page - 1;
+          if (prevPage < 1) prevPage = pageCount;
+          handlePageChange(prevPage)
+        }}>PREV</button>
+        {pages.map((pageNumber) => {
+          return <button type="button" key={pageNumber} className={`btn btn-xs sm:btn-md border-none join-item ${page === pageNumber ? 'bg-base-300 border-base-300' : ''}`} onClick={() => {
+            handlePageChange(pageNumber);
+          }}>{pageNumber}</button>
+        })}
+        <button type="button" className="btn btn-xs sm:btn-md join-item" onClick={() => {
+          let nextPage = page + 1;
+          if (nextPage > pageCount) nextPage = 1;
+          handlePageChange(nextPage);
+        }}>NEXT</button>
+      </div>
+    </div>
   )
 }
 
